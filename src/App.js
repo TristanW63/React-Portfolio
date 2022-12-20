30ab564767abafbd98f8a55ff9c7ecc1942a19a9
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-20 15:30:29 -0600

    intro and animations

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Home from "./components/intro/intro";
+
+const App = () => {
+    return <div><Home/></div>;
+  };
+  
+  export default App;
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Intro from "./components/intro/intro";
-
-const App = () => {
-    return <div><Intro/></div>;
-  };
-  
-  export default App;
