8055f787c9c7a5ff902048bb12c5b3d02b91bd05
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-12-25 13:05:03 -0600

    contact styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,21 +8,24 @@ function Contact() {
   return (
     <Container fluid className='form'>
         <Container>
+            <h1 className='contact-heading' style={{ textAlign: "center", color: "white" }}>
+                Feel free to contact me about any inquires!
+            </h1>
     <Form>
       <Row>
-        <Col>
+        <Col className='names'>
           <Form.Control placeholder="First name" />
         </Col>
-        <Col>
+        <Col className='names'>
           <Form.Control placeholder="Last name" />
         </Col>
       </Row>
       <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label className='formText'>Email address</Form.Label>
+        <Form.Control type="email" placeholder="example@email.com" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
+        <Form.Label className='formText'>Send me a message</Form.Label>
         <Form.Control as="textarea" rows={3} />
       </Form.Group>
     </Form>
